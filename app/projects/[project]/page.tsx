f337550ea3821cab04bd94c6e55bfc4bfe981625
patlo-git <56564279+patlo-git@users.noreply.github.com>
2023-12-14 17:35:46 -0800

    Add styling and project link to project page

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -8,5 +8,22 @@ export default async function Project({ params }: Props) {
   const slug = params.project;
   const project = await getProject(slug);
 
-  return <div>{project.name}</div>;
+  return (
+    <div className="max-w-3xl mx-auto py-20">
+      <header className="flex items-center justify-between px-10">
+        <h1 className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-7xl font-extralight">
+          {project.name}
+        </h1>
+        <a
+          href={project.url}
+          title="View Project"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition"
+        >
+          View Project
+        </a>
+      </header>
+    </div>
+  );
 }
