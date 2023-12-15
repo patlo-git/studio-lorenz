913533a6a83c0f4d59a07c38aceb6754cf02903e
patlo-git <56564279+patlo-git@users.noreply.github.com>
2023-12-14 16:41:34 -0800

    Create dynamic route for project page

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -22,3 +22,26 @@ export async function getProjects(): Promise<Project[]> {
     }`
   );
 }
+
+export async function getProject(slug: string): Promise<Project> {
+  const client = createClient({
+    projectId: 'ltuarcsn',
+
+    dataset: 'production',
+
+    apiVersion: '2023-12-12',
+  });
+
+  return client.fetch(
+    groq`*[_type == 'project' && slug.current == $slug ][0] {
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+    }`,
+    { slug }
+  );
+}
