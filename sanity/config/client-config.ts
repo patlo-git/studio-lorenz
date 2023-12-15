dbc867a9b03f3572e411edbf0de2539ba7033f7f
patlo-git <56564279+patlo-git@users.noreply.github.com>
2023-12-14 16:53:05 -0800

    Move client config out of Sanity utils

---
+++ sanity/config/client-config.ts
@@ -0,0 +1,9 @@
+const config = {
+  projectId: 'ltuarcsn',
+
+  dataset: 'production',
+
+  apiVersion: '2023-12-12',
+};
+
+export default config;
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,16 +1,9 @@
 import { Project } from '@/types/Project';
 import { createClient, groq } from 'next-sanity';
+import config from './config/client-config';
 
 export async function getProjects(): Promise<Project[]> {
-  const client = createClient({
-    projectId: 'ltuarcsn',
-
-    dataset: 'production',
-
-    apiVersion: '2023-12-12',
-  });
-
-  return client.fetch(
+  return createClient(config).fetch(
     groq`*[_type == 'project']{
       _id,
       _createdAt,
@@ -24,15 +17,7 @@ export async function getProjects(): Promise<Project[]> {
 }
 
 export async function getProject(slug: string): Promise<Project> {
-  const client = createClient({
-    projectId: 'ltuarcsn',
-
-    dataset: 'production',
-
-    apiVersion: '2023-12-12',
-  });
-
-  return client.fetch(
+  return createClient(config).fetch(
     groq`*[_type == 'project' && slug.current == $slug ][0] {
       _id,
       _createdAt,
