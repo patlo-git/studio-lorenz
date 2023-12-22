4141b0bf8c282959248362a4933c2406b204279e
patlo-git <56564279+patlo-git@users.noreply.github.com>
2023-12-22 18:47:37 -0500

    Add styling and Image to Project page

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,5 +1,6 @@
 import { getProject } from '@/sanity/sanity-utils';
 import { PortableText } from '@portabletext/react';
+import Image from 'next/image';
 
 type Props = {
   params: { project: string };
@@ -26,7 +27,17 @@ export default async function Project({ params }: Props) {
         </a>
       </header>
 
-      <PortableText value={project.content} />
+      {/* content goes here */}
+      <div className="text-lg text-gray-700 mt-5 p-10">
+        <PortableText value={project.content} />
+        <Image
+          src={project.image}
+          alt={project.name}
+          width={1920}
+          height={1080}
+          className="mt-10 border-2 border-gray-700 object-cover rounded-l"
+        />
+      </div>
     </div>
   );
 }
